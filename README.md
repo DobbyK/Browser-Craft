ef2717dc3b322d30278a184438913265de8f3501
Dobby King <dobbyk@techie.com>
2021-01-30 01:08:14 +0000

    Started working on tile, and player. Probally some random updates here too

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Browser-Craft
-Welcome to OreCraft! Play it your browser for free: http://orecraft.dobbyk.repl.co
+Welcome to OreCraft! Play it your browser for free: http://OreCraft.dobbyk.repl.co
--- play/game/script.js
+++ play/game/script.js
@@ -19,23 +19,21 @@ var config = {
 var game = new Phaser.Game(config);
 var blocks;
 function preload (){
-	this.load.image('background', '../../static/background.png');
 	this.load.spritesheet('player', '../../static/player_sprite.png', { frameWidth: 230, frameHeight: 300 });
 	this.load.spritesheet("inventory", "../../static/inventory.png", { frameWidth: 96, frameHeight: 96 });
-	this.load.image("map-tiles-2", "../../static/tileset2.png");
+	this.load.image("map-tiles", "../../static/tileset.png");
 }
 
 function create (){
 	// Variables
 	grass = 0;
-	water = 0;
 	dirt = 0;
 	stone = 0;
-	chiseled_stone = 0;
 	lastdirection = 'right';
 	alert('Arrow Keys to move. Press 1-4 to switch items, right click to remove and left click to add. And Press I to view inventory.')
 	objectToPlace = 'grass';
 	grassHeld = false;
+	click = 1;
 	// World Gen
 	world = [
 		[-1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,-1],
@@ -49,7 +47,7 @@ function create (){
 		[-1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,-1],
 		[-1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,-1],
 		[-1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,-1],
-		[-1,0,0,0,0,0,0,0,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,-1],
+		[-1,0,0,0,0,0,0,0,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,10,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,-1],
 		[-1,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,-1],
 		[-1,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,-1],
 		[-1,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,-1],
@@ -64,7 +62,7 @@ function create (){
 	// Main Rendering
 	this.cameras.main.zoom = 0.7;
 	map = this.make.tilemap({data: world, tileWidth: 142.9, tileHeight: 142.9});
-	tiles = map.addTilesetImage("map-tiles-2");
+	tiles = map.addTilesetImage("map-tiles");
 	layer = map.createLayer(0, tiles, 0, 0);
 	this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 	map.setCollision([-1, 6, 7, 1, 2]);
@@ -77,26 +75,22 @@ function create (){
 	player.setCollideWorldBounds(false);
 	player.body.setGravityY(0);
 	this.cameras.main.startFollow(player);
-	inventory1 = this.add.sprite(player.x, player.y - 300, 'inventory');
-	inventory2 = this.add.sprite(player.x -96, player.y - 300, 'inventory');
-	inventory3 = this.add.sprite(player.x -192, player.y - 300, 'inventory');
-	inventory4 = this.add.sprite(player.x -288, player.y - 300, 'inventory');
-	inventory5 = this.add.sprite(player.x +96, player.y - 300, 'inventory');
-	grassint = this.add.text(player.x, player.y - 275, grass);
-	dirtint = this.add.text(player.x -96, player.y - 250, dirt);
-	waterint = this.add.text(player.x -192, player.y - 250, water);
-	stoneint = this.add.text(player.x -288, player.y - 250, stone);
-	chiseled_stoneint = this.add.text(player.x +96, player.y - 250, chiseled_stone);
+	inventory1 = this.add.sprite(player.x, player.y + 300, 'inventory');
+	inventory2 = this.add.sprite(player.x + 96, player.y + 300, 'inventory');
+	inventory3 = this.add.sprite(player.x + 192, player.y + 300, 'inventory');
+	grassint = this.add.text(player.x, player.y - 250, grass);
+	dirtint = this.add.text(player.x + 96, player.y - 250, dirt);
+	stoneint = this.add.text(player.x +192, player.y - 250, stone);
 	// Animations and Some Inputing
 	player.anims.create({
         key: 'walk-right',
-        frames: this.anims.generateFrameNumbers('player', {frames: [2, 0]} ),
+        frames: this.anims.generateFrameNumbers('player', {frames: [2, 1, 0, 1]} ),
         frameRate: 6,
         repeat: -1
     });
 	player.anims.create({
         key: 'walk-left',
-        frames: this.anims.generateFrameNumbers('player', {frames: [3, 5]} ),
+        frames: this.anims.generateFrameNumbers('player', {frames: [3, 4, 5, 4]} ),
         frameRate: 6,
         repeat: -1
     });
@@ -112,6 +106,7 @@ function create (){
         frameRate: 6,
         repeat: -1
     });
+	player.anims.play('stand-right', true);
 	inventory1.anims.create({
 		key: 'nothing',
         frames: this.anims.generateFrameNumbers('inventory', {frames: [4, 4]} ),
@@ -143,32 +138,8 @@ function create (){
         repeat: -1
 	});
 	inventory3.anims.create({
-		key: 'water',
-        frames: this.anims.generateFrameNumbers('inventory', {frames: [2, 2]} ),
-        frameRate: 6,
-        repeat: -1
-	});
-	inventory4.anims.create({
-		key: 'nothing',
-        frames: this.anims.generateFrameNumbers('inventory', {frames: [4, 4]} ),
-        frameRate: 6,
-        repeat: -1
-	});
-	inventory4.anims.create({
 		key: 'stone',
         frames: this.anims.generateFrameNumbers('inventory', {frames: [3, 3]} ),
-        frameRate: 6,
-        repeat: -1
-	});
-	inventory5.anims.create({
-		key: 'chiseled_stone',
-        frames: this.anims.generateFrameNumbers('inventory', {frames: [3, 3]} ),
-        frameRate: 6,
-        repeat: -1
-	});
-	inventory5.anims.create({
-		key: 'nothing',
-        frames: this.anims.generateFrameNumbers('inventory', {frames: [4, 4]} ),
         frameRate: 6,
         repeat: -1
 	});
@@ -180,32 +151,71 @@ function create (){
         objectToPlace = 'dirt';
 		console.log(objectToPlace);
     });
-    this.input.keyboard.on('keydown-THREE', function (event) {
+	this.input.keyboard.on('keydown-FOUR', function (event) {
         objectToPlace = 'stone';
 		console.log(objectToPlace);
     });
-	this.input.keyboard.on('keydown-FOUR', function (event) {
-        objectToPlace = 'water';
-		console.log(objectToPlace);
-    });
-	cursors.up.on('down', function () {
-        if (player.body.blocked.down)
-        {
+	this.input.keyboard.on('keydown-W', function (event) {
+        if (player.body.blocked.down) {
             player.body.setVelocityY(-420);
         }
-    }, this);
+    });
+	this.input.keyboard.on('keydown-A', function (event) {
+        player.setVelocityX(-320);
+		player.anims.play('walk-left', true);
+		lastdirection = 'left';
+    });
+	this.input.keyboard.on('keydown-D', function (event) {
+        player.setVelocityX(320);
+		player.anims.play('walk-right', true);
+		lastdirection = 'right';
+    });
+	this.input.keyboard.on('keydown-E', function (event){
+		if (click == 1) {
+			console.log('Click 1');
+			click = 2;
+		} else if (click == 2) {
+			console.log('Click 2');
+			click = 1;
+		}
+	});
+	this.input.keyboard.on('keyup-A', function (event) {
+        player.setVelocityX(0);
+		player.anims.play('stand-left', true);
+    });
+	this.input.keyboard.on('keyup-D', function (event) {
+        player.setVelocityX(0);
+		player.anims.play('stand-right', true);
+    });
 	this.input.mouse.disableContextMenu();
 	this.input.on('pointerdown', function (pointer) {
 		if (pointer.rightButtonDown()) {
-			tile = map.getTileAt(pointerTileX, pointerTileY).index;
+			tile = map.getTileAt(pointerTileX, pointerTileY);
+			tilet = tile.index;
+			playertile = map.worldToTileXY(player.x, player.y);
+			console.log(tile.x, playertile.x)
 			if (tile == -1) {
 				objectToPlace = 'Null';
 				switch(objectToPlace) {
 					case 'Null':
 						break;
 				}
+			} 
+			if (tile.x !== playertile.x) {
+				objectToPlace = 'Null';
+				switch(objectToPlace) {
+					case 'Null':
+						break;
+				}
+			} 
+			if (tile.y == playertile.y) {
+				objectToPlace = 'Null';
+				switch(objectToPlace) {
+					case 'Null':
+						break;
+				}
 			} else {
-				switch(tile) {
+				switch(tilet) {
 					case 6:
 						if (grass > 20) {
 							break;
@@ -222,14 +232,6 @@ function create (){
 							map.putTileAt(4, pointerTileX, pointerTileY);
 							break;
 						}
-					case 0:
-						if (water > 20) {
-							break;
-						} else {
-							water += 1;
-							map.putTileAt(4, pointerTileX, pointerTileY);
-							break;
-						}
 					case 1:
 						if (stone > 20) {
 							break;
@@ -237,6 +239,12 @@ function create (){
 							stone += 1;
 							map.putTileAt(4, pointerTileX, pointerTileY);
 							break;
+						}
+					case 10:
+						if (click == 1) {
+							alert('This is a chest!');
+						} else if (click == 2) {
+							map.putTileAt(4, pointerTileX, pointerTileY);
 						}
 				}
 			}
@@ -264,15 +272,6 @@ function create (){
 						dirt += -1;
 					}
 					break;
-				case 'water':
-					if (tile !== 4) {
-						break;
-					}
-					if (water > 0) {
-						map.putTileAt(0, pointerTileX, pointerTileY);
-						water += -1;
-					}
-					break;
 				case 'stone':
 					if (tile !== 4) {
 						break;
@@ -295,49 +294,20 @@ function update (){
 	pointerTileX = map.worldToTileX(worldPoint.x);
     pointerTileY = map.worldToTileY(worldPoint.y);
 	inventory1.x = player.x;
-	inventory1.y = player.y - 300;
-	inventory2.x = player.x - 96;
-	inventory2.y = player.y - 300;
-	inventory3.x = player.x - 192;
-	inventory3.y = player.y - 300;
-	inventory4.x = player.x - 288;
-	inventory4.y = player.y - 300;
-	inventory5.x = player.x + 96;
-	inventory5.y = player.y - 300;
-	inventory2.anims.play('nothing', true);
-	inventory3.anims.play('nothing', true);
-	inventory4.anims.play('nothing', true);
+	inventory1.y = player.y + 300;
+	inventory2.x = player.x + 96;
+	inventory2.y = player.y + 300;
+	inventory3.x = player.x + 192;
+	inventory3.y = player.y + 300;
 	grassint.x = player.x;
-	grassint.y = player.y - 250;
+	grassint.y = player.y + 250;
 	grassint.text = grass;
-	dirtint.x = player.x - 96 ;
-	dirtint.y = player.y - 250;
+	dirtint.x = player.x + 96 ;
+	dirtint.y = player.y + 250;
 	dirtint.text = dirt;
-	waterint.x = player.x - 192;
-	waterint.y = player.y - 250;
-	waterint.text = water;
-	stoneint.x = player.x - 288;
-	stoneint.y = player.y - 250;
+	stoneint.x = player.x + 192;
+	stoneint.y = player.y + 250;
 	stoneint.text = stone;
-	chiseled_stoneint.x = player.x + 96;
-	chiseled_stoneint.y = player.y - 250;
-	chiseled_stoneint.text = chiseled_stone;
-	if (cursors.left.isDown){
-    	player.setVelocityX(-320);
-		player.anims.play('walk-left', true);
-		lastdirection = 'left';
-	} else if (cursors.right.isDown) {
-    	player.setVelocityX(320);
-		player.anims.play('walk-right', true);
-		lastdirection = 'right';
-	} else {
-   		player.setVelocityX(0);
-		if (lastdirection == 'right') {
-			player.anims.play('stand-right', true);
-		} else if (lastdirection == 'left') {
-			player.anims.play('stand-left', true);
-		}
-	} 
 	if (grass > 0) {
 		inventory1.anims.play('grass', true);
 	} else {
@@ -348,19 +318,9 @@ function update (){
 	} else {
 		inventory2.anims.play('nothing', true);
 	}
-	if (water > 0) {
-		inventory3.anims.play('water', true);
+	if (stone > 0) {
+		inventory3.anims.play('stone', true);
 	} else {
 		inventory3.anims.play('nothing', true);
-	}
-	if (stone > 0) {
-		inventory4.anims.play('stone', true);
-	} else {
-		inventory4.anims.play('nothing', true);
 	} 
-	if (chiseled_stone > 0) {
-		inventory5.anims.play('chiseled_stone', true);
-	} else {
-		inventory5.anims.play('nothing', true);
-	}
 }
